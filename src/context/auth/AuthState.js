74075c43e970f8954bb2d094d165ea2592b6d2f3
Muhammad Mahmoud Agha <muha.agha@gmail.com>
2020-10-26 17:18:53 +0100

    Update AuthState.js

--- src/context/auth/AuthState.js
+++ src/context/auth/AuthState.js
@@ -30,7 +30,7 @@ const AuthState = props => {
     setAuthToken(localStorage.token);
 
     try {
-      const res = await axios.get('/api/auth');
+      const res = await axios.get('https://nicetwicede.herokuapp.com/api/auth');
 
       dispatch({
         type: USER_LOADED,
@@ -50,7 +50,7 @@ const AuthState = props => {
     };
 
     try {
-      const res = await axios.post('/api/users', formData, config);
+      const res = await axios.post('https://nicetwicede.herokuapp.com/api/users', formData, config);
 
       dispatch({
         type: REGISTER_SUCCESS,
@@ -75,7 +75,7 @@ const AuthState = props => {
     };
 
     try {
-      const res = await axios.post('/api/auth', formData, config);
+      const res = await axios.post('https://nicetwicede.herokuapp.com/api/auth', formData, config);
 
       dispatch({
         type: LOGIN_SUCCESS,
