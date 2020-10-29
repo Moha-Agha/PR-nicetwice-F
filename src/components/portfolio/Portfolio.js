9ed81180694ea611314decc53c82c5e6e46d61d0
Muhammad Mahmoud Agha <muha.agha@gmail.com>
2020-10-29 16:38:57 +0100

    updat protfolio

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,61 +1,23 @@
-import React, { useState, useEffect, Fragment } from "react";
+import React from "react";
 
-import Preloader from "../layout/Preloader"
 import PortfolioItem from "./PortfolioItem"
 
 const Portfolio = () => {
 
-  const [portfolioItems, setPortfolioItems] = useState([]);
-  const [loading, setLoading] = useState(true);
+  return (
+    <div id="portfolio" >
 
+      <h1 className="portfolio_title"> Portfolio </h1>
+      <p className="portfolio_subtitle">UI-UX design website and App programming</p>
 
-  useEffect(() => {
-    window.addEventListener('resize', resizeScreen)
-    resizeScreen()
-  }, []);
-
-  function resizeScreen() {
-    let Items
-
-    if (window.innerWidth <= 750) {
-      Items = <Fragment>
-        < PortfolioItem number={1} image={'portfolio-1'} type='.jpg' href='#' />
-        < PortfolioItem number={2} image={'portfolio-2'} type='.jpg' href='https://that.im/' />
-        < PortfolioItem number={3} image={'portfolio-3'} type='.jpg' href='https://inislam.net/' />
-        < PortfolioItem number={4} image={'portfolio-4'} type='.jpg' content='false' />
-      </Fragment>
-    } else {
-      Items = <Fragment>
-        < PortfolioItem number={2} image={'portfolio-2'} type='.jpg' href='https://that.im/' />
-        < PortfolioItem number={4} image={'portfolio-4'} type='.jpg' content='false' />
-        < PortfolioItem number={1} image={'portfolio-1'} type='.jpg' href='#' />
-        < PortfolioItem number={3} image={'portfolio-3'} type='.jpg' href='https://inislam.net/' />
-      </Fragment>
-    }
-
-    setPortfolioItems(Items)
-    setLoading(false)
-  }
-
-
-  if (loading) {
-    return <Preloader />
-  } else {
-    return (
-      <div id="portfolio" >
-
-        <h1 className="portfolio_title"> Portfolio </h1>
-        <p className="portfolio_subtitle">UI-UX design website and App programming</p>
-
-        <div className='portfolio'>
-          < PortfolioItem number={1} image={'portfolio-1'} type='.jpg' href='https://that.im/' />
-          < PortfolioItem number={2} image={'portfolio-2'} type='.jpg' href='https://inislam.net/' />
-          < PortfolioItem number={3} image={'portfolio-3'} type='.jpg' href='#' />
-          < PortfolioItem number={4} image={'portfolio-4'} type='.jpg' href='#' />
-        </div>
+      <div className='portfolio'>
+        < PortfolioItem number={1} image={'portfolio-1'} type='.jpg' href='https://that.im/' />
+        < PortfolioItem number={2} image={'portfolio-2'} type='.jpg' href='https://inislam.net/' />
+        < PortfolioItem number={3} image={'portfolio-3'} type='.jpg' href='#' />
+        < PortfolioItem number={4} type='.jpg' href='#' />
       </div>
-    )
-  }
+    </div>
+  )
 
 };
 
--- src/components/portfolio/PortfolioItem.js
+++ src/components/portfolio/PortfolioItem.js
@@ -1,41 +1,30 @@
 import React from "react";
 
-const PortfolioItem = ({ number = '', image = '', type = '.jpg', href = '', content = true }) => {
+const PortfolioItem = ({ number = '', image = '', type = '.jpg', href = '#' }) => {
 
-  let cardContent
   let img
   let path
 
   if (image !== '') {
     path = require('../../media/image/' + image + type)
-
-    img = <img src={path} alt="niceTwice.de portfolio" />
-  } else {
-    img = <h1 className='text-center pt-5'>no image</h1>
-  }
-
-  if (content === true) {
-    cardContent = < a href={href} target="_blank" rel="noopener noreferrer" className="portfolio-item-img" >
-      {img}
-    </a >
-  } else {
-    cardContent = <span className='portfolio-asking'>
-      <div className='asking_first-layer'>
-        <h2>Ein freier Platz für<br /> dein business!</h2>
+    img = (
+      <div className='portfolio-item_card'>
+        < a href={href} target="_blank" rel="noopener noreferrer" className="portfolio-item-img" >
+          <img src={path} alt="niceTwice.de portfolio" />
+        </a>
       </div>
-      <span className='asking_second-layer'>
-        {img}
-      </span>
-    </span >
+    )
+  } else {
+    img = <div className='free-place'><div><h2>EIN FREIER PLATZ<br />FÜR DEIN BUSINESS!</h2></div></div>
   }
+
+
 
 
   return (
     <div className='portfolio-item'>
       <span className='portfolio-item_number'>{number}</span>
-      <div className='portfolio-item_card'>
-        {cardContent}
-      </div>
+      {img}
     </div>
   );
 };
