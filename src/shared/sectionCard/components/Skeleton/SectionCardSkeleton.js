dd7d840c181f46c31b4cabc15042220d5f299810
Muhammad Mahmoud Agha <muha.agha@gmail.com>
2021-03-09 15:29:33 +0100

    Update SectionCardSkeleton.js

--- src/shared/sectionCard/components/Skeleton/SectionCardSkeleton.js
+++ src/shared/sectionCard/components/Skeleton/SectionCardSkeleton.js
@@ -4,22 +4,22 @@ const SectionCardSkeleton = ({classStyle}) => {
     return (
         <>
             <div className={classStyle ? "section-card-skeleton "+classStyle : "section-card-skeleton"}>
-                <div class="section-card-skeleton_shine"></div>
+                <div className="section-card-skeleton_shine"></div>
             </div>
             <div className={classStyle ? "section-card-skeleton "+classStyle : "section-card-skeleton"}>
-                <div class="section-card-skeleton_shine"></div>
+                <div className="section-card-skeleton_shine"></div>
             </div>
             <div className={classStyle ? "section-card-skeleton "+classStyle : "section-card-skeleton"}>
-                <div class="section-card-skeleton_shine"></div>
+                <div className="section-card-skeleton_shine"></div>
             </div>
             <div className={classStyle ? "section-card-skeleton "+classStyle : "section-card-skeleton"}>
-                <div class="section-card-skeleton_shine"></div>
+                <div className="section-card-skeleton_shine"></div>
             </div>
             <div className={classStyle ? "section-card-skeleton "+classStyle : "section-card-skeleton"}>
-                <div class="section-card-skeleton_shine"></div>
+                <div className="section-card-skeleton_shine"></div>
             </div>
             <div className={classStyle ? "section-card-skeleton "+classStyle : "section-card-skeleton"}>
-                <div class="section-card-skeleton_shine"></div>
+                <div className="section-card-skeleton_shine"></div>
             </div>
         </>
     )
